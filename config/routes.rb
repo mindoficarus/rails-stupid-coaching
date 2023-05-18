6c19dcde054f5833a2e5c1ee0a7aee2456cb434f
Felipe Mandiola <hi@icarusmind.com>
2023-05-18 19:17:47 -0400

    listo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root                   to: "pages#home"
-  get "about",           to: "pages#about",      as: :about
-  get "contact",         to: "pages#contact",    as: :contact
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
 end
